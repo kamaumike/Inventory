7244a4e34027fb008218389db843d1c7f6c408d6
kamaumike <kamaumike2013@gmail.com>
2017-01-14 21:07:13 +0300

    Added instances of Migrate & Manager

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from run import app
+
+
+# Create an instance of Migrate
+migrate = Migrate(app, db)
+
+# Create an instance of Manager
+manager = Manager(app)
+
+# Connect the script manager and flask-migrate.
+#'db' is a command to be run
+manager.add_command('db', MigrateCommand)
+
+# Start the script manager
+if __name__ == '__main__':
+    manager.run()
