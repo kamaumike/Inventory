546e407fac28f8c260a6417e6e511090e52b726a
kamaumike <kamaumike2013@gmail.com>
2017-01-15 13:30:23 +0300

    Created & initialized the LoginManager object

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import app_config
+from flask_login import LoginManager
 
 # Create an SQLAlchemy object
 db = SQLAlchemy()
+
+# Create a LoginManager object
+login_manager = LoginManager()
 
 
 def create_app(config_name):
@@ -18,6 +22,14 @@ def create_app(config_name):
     # Get the environment configurations to load
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
+
+    # Initialize the SQLAlchemy object
     db.init_app(app)
+
+    # Initialize the LoginManager object
+    login_manager.init_app(app)
+
+    # Redirect users to the login page
+    login_manager.login_view = "auth.login"
 
     return app
