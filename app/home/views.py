1a238c00d3f1f79fb6548a13bd53da76bec69cdc
kamaumike <kamaumike2013@gmail.com>
2017-01-17 17:04:51 +0300

    Added dashboard() to render the dashboard.html template

--- app/home/views.py
+++ app/home/views.py
@@ -9,3 +9,12 @@ def index():
     Render the index template
     """
     return render_template('home/index.html', title="Welcome")
+
+
+@home.route('/dashboard')
+@login_required
+def dashboard():
+    """
+    Render the dashboard template
+    """
+    return render_template('home/dashboard.html', title="Dashboard")
