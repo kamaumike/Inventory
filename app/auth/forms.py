97ecce740a2795b539f6b8c3ee031e02b874d58b
kamaumike <kamaumike2013@gmail.com>
2017-01-19 21:39:34 +0300

    Added the Sign Up form

---
+++ app/auth/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email
+from ..models import User
+
+
+class SignUpForm(FlaskForm):
+    """
+    The Sign Up Form
+    """
+    firstname = StringField('Firstname', validators=[DataRequired()])
+    lastname = StringField('Lastname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, field):
+        """
+        Verify if email already exists in the database
+        """
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError(
+                'Email is already exists! Use a different email.')
